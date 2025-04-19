f14029da51627a36107b3a5160673e82221a2247
markkkjessus <mark.dejesus3211@gmail.com>
2025-04-19 18:46:49 +0800

    refactor: update dependencies and remove unused stores
    - Updated package.json to remove dark mode settings and upgrade dependencies including React, React Native, and Expo.
    - Deleted unused authStore and cartStore files to clean up the codebase.
    - Removed tailwind.config.js as it is no longer needed.
    - Simplified tsconfig.json by removing unnecessary paths configuration.
    - Added a new package-lock.json file to ensure consistent installs.

--- ecommerce-mobile/app/index.tsx
+++ ecommerce-mobile/app/index.tsx
@@ -1,39 +1,9 @@
-import { ActivityIndicator, FlatList } from 'react-native';
-import ProductListItem from '../components/ProductListItem';
-import { useBreakpointValue } from '@/components/ui/utils/use-break-point-value';
-import { listProducts } from '@/api/products';
-import { useQuery } from '@tanstack/react-query';
-import { err } from 'react-native-svg/lib/typescript/xml';
-import { Text } from '@/components/ui/text';
+import { View, Text} from "react-native";
 
-export default function HomeScreen() {
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['products'],
-    queryFn: listProducts,
-  });
-
-  const numColumns = useBreakpointValue({
-    default: 2,
-    sm: 3,
-    xl: 4,
-  });
-
-  if (isLoading) {
-    return <ActivityIndicator />;
-  }
-
-  if (error) {
-    return <Text>Error fetching products</Text>;
-  }
-
-  return (
-    <FlatList
-      key={numColumns}
-      data={data.splice(0, 10)}
-      numColumns={numColumns}
-      contentContainerClassName="gap-2 max-w-[960px] mx-auto w-full"
-      columnWrapperClassName="gap-2"
-      renderItem={({ item }) => <ProductListItem product={item} />}
-    />
+export default function HomeScreen(){
+  return(
+    <View>
+      <Text style={{ fontSize: 30}}>Home Screen</Text>
+    </View>
   );
 }
